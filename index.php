31ad3fef8a5cc9ad6c208c99adf5a5f302ba4306
jaenudin0901 <jaenudinj913198@gmail.com>
2021-06-08 03:18:18 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,235 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   <center>
+        <h1>PT.MAJU BERSAMA</h1>
+        <h2>SLIP GAJI KARYAWAN</h2>
+        <hr style="50%; border-width=5px; color:black;">
+    </center>
+    <div class="container">
+
+
+    
+  <div class="row">
+    <div class="col-sm-6">
+                    <table>
+            <tr>
+                <td>Nama</td>
+                <td  >:</td>
+                <!-- <td><?= $ps['nama']; ?></td> -->
+                <td> Jaenduin</td>
+            </tr>
+            <tr>
+                <td>Nik</td>
+                <td>:</td>
+                <!-- <td><?= $ps['nik']; ?></td> -->
+                <td>   12140085</td>
+            </tr>
+            <tr>
+                <td>Jabatan</td>
+                <td>:</td>
+                <!-- <td><?= $ps['nama_jabatan']; ?></td> -->
+                <td>    Security</td>
+            </tr>
+            <tr>
+                <td>Bulan</td>
+                <td>:</td>
+                <!-- <td><?= substr($ps['bulan'], 0, 2); ?></td> -->
+                <td>  juni</td>
+            </tr>
+            <tr>
+                <td>Tahun</td>
+                <td>:</td>
+                <!-- <td><?= substr($ps['bulan'], 2, 4); ?></td> -->
+                <td>    2021</td>
+            </tr>
+        </table>
+     
+    </div>
+    <div class="col-sm-6">
+                <table>
+            <tr>
+                <td>Tax Ref</td>
+                <td>    </td>
+                <td>:</td>
+                <!-- <td><?= $ps['nama']; ?></td> -->
+                <td> 0</td>
+            </tr>
+            <tr>
+                <td>Tax Stats</td>
+                <td>    </td><td>:</td>
+                <!-- <td><?= $ps['nik']; ?></td> -->
+                <td>KL/1</td>
+            </tr>
+        </table>
+    </div>
+  
+  </div>
+  <div class="row"> 
+    <div class="col-sm-6">
+        <table class="table">
+  <thead>
+    <tr>
+        <center>    
+      
+      <th class="text-center">Income</th>
+      </center>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">Sallary</td>
+      <td>5000000</td>
+     
+    </tr>
+    <tr>
+      <td scope="row">Transport</td>
+      <td>300000</td>
+    
+    </tr>
+    <tr>
+      <td scope="row">Uang Makan</td>
+      <td>500000</td>
+     
+    </tr>
+    <tr>
+      <td scope="row">Lemburan</td>
+      <td>500000</td>
+     
+    </tr> <tr>
+      <th scope="row">Total Income</th>
+      <th>6300000</th>
+     
+    </tr>
+  </tbody>
+</table>
+        
+    </div>
+    <div class="col-sm-6">
+            <table class="table">
+  <thead>
+    <tr>
+        <center>    
+      
+      <th class="text-center">Dedecution</th>
+      </center>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">BPJS TK 5.7%</td>
+      <td>5000000</td>
+     
+    </tr>
+    <tr>
+      <td scope="row">BPJS KES %</td>
+      <td>3000000</td>
+    
+    </tr>
+    <tr>
+      <td scope="row">Potongan Absensi</td>
+      <td>0</td>
+     
+    </tr>
+
+     <tr>
+      <th scope="row">Dedecution</th>
+      <th>800000</th>
+     
+    </tr> 
+
+  </tbody>
+</table>
+    </div>
+      <table class="table">
+  <thead>
+    <tr>
+        <center>    
+      
+      <!-- <th class="text-center">Dedecution</th> -->
+      </center>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"  >NET PAY</th>
+      <th >5500000</th>
+     
+    </tr></tbody>
+    
+</table>
+  
+ <div class="col-sm-6">  
+    <table class="table">
+      <thead>
+ <tr>
+        <center>    
+      
+      <th class="text-center">Attendance Summary</th>
+      </center>
+     
+    </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td scope="row"  >ABS (Absent)</td>
+          <td >0</td>
+         
+        </tr> <tr>
+          <td scope="row"  >PRS (Present)</td>
+          <td >22</td>
+         
+        </tr>
+
+    </tbody>
+</table>   
+</div>
+ <div class="col-sm-6">   
+    <table class="table">
+      <thead>
+      
+        <center>    
+      
+      <th class="text-center">Attendance Summary</th>
+      </center>
+     
+      </thead>
+      <tbody>
+        <tr>
+          <td scope="row"  >OVT_INDEX (Index Overtime)</td>
+          <td >0</td>
+         
+        </tr> 
+
+    </tbody>
+</table>  
+</div>
+</div>  
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
